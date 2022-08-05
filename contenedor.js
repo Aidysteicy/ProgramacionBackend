287f45fec5bdf0a04a463aeeff0b48baa02add82
Aidysteicy <96146121+Aidysteicy@users.noreply.github.com>
2022-08-04 23:46:35 -0300

    Add files via upload

---
+++ contenedor.js
@@ -0,0 +1,102 @@
+const fs = require('fs').promises;
+
+class Contenedor {
+
+    constructor( ruta){
+        this.ruta = ruta;
+        this.fs = fs;
+    }
+
+    async save(objeto){
+        let id = 1
+        try {
+            let productos = await this.getAll();
+            if(productos === undefined){
+                productos = await this.fs.writeFile(this.ruta, JSON.stringify([{...objeto, id: id}], null, 2))
+            }else{
+                productos.forEach(() => {
+                    if(productos.find(prod => prod.id === id)){
+                        id++;
+                    }
+                });
+            await this.fs.writeFile(this.ruta, JSON.stringify([...productos, {...objeto, id: id}], null, 2))
+            }
+        return id;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async saveByID(id, objeto){
+        try {
+            let productos = await this.getAll();
+            const prodID = productos.findIndex(prod => prod.id === id)
+            if(prodID!==-1){
+                productos[prodID]={...objeto, id:id}
+                await this.fs.writeFile(this.ruta, JSON.stringify(productos, null, 2))
+                return {msg: 'Producto Modificado'}
+            }else{
+                return {error: 'Producto no encontrado'}
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getbyId(id){
+        const productos = await this.getAll();
+        const prodID = productos.find(prod => prod.id === id)
+        if(prodID){
+            return prodID; 
+        }
+        console.log('No existe un producto con ese ID')
+        return {error: 'Producto no encontrado'}
+    }
+
+    async getAll(){
+        try {
+            const productos = await this.fs.readFile(this.ruta, 'utf-8');
+            const prodJson = JSON.parse(productos);
+            return prodJson;
+        } catch (error) {
+            console.log('Archivo no Existe')
+        }
+    }
+
+    async deleteById(id){
+        try {
+            const productos = await this.getAll();
+            const prodID = productos.find(prod => prod.id === id)
+            console.log(prodID)
+            if(prodID){
+                const nuevos_productos = productos.filter(prod => prod.id !== id)
+                await this.fs.writeFile(this.ruta, JSON.stringify(nuevos_productos, null, 2))
+            }else{
+                return {error: 'Producto no encontrado'}
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async deleteAll(){
+        try {
+            await this.fs.writeFile(this.ruta, [])
+            console.log('Todos los productos fueron borrados')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getIDRandom(){
+        try {
+            let productos = await this.getAll();
+            let num = Math.floor(Math.random()*productos.length)+1;
+            return await this.getbyId(num);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = Contenedor;
---
+++ server.js
@@ -0,0 +1,62 @@
+const Contenedor = require('./contenedor')
+const contenedor = new Contenedor('./files/productos.txt');
+
+const express = require('express');
+const app = express();
+
+const {Router} = express
+const rutaProductos = Router()
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use('/api/productos', rutaProductos)
+
+rutaProductos.get('/', async (req,res)=>{
+    const prod = await contenedor.getAll()
+    res.send(prod)
+});
+
+rutaProductos.get('/:id', async (req,res)=>{
+    const {id} = req.params
+    const prod = await contenedor.getbyId(+id);
+    res.json(prod)
+});
+
+
+rutaProductos.post('/', async (req,res)=>{
+    const producto = req.body
+    const id = await contenedor.save(producto)
+    prod = {...producto, id: id}
+    res.json({
+        msg: 'Producto Agregado',
+        prod
+    })
+});
+
+rutaProductos.put('/:id', async (req,res)=>{
+    const {id}=req.params
+    const prod = req.body
+    const mod = await contenedor.saveByID(+id, prod)
+    res.json(mod)
+});
+
+rutaProductos.delete('/api/productos/:id', async (req,res)=>{
+    const {id}=req.params
+    const prod = await contenedor.deleteById(+id)
+    console.log(prod)
+    res.json(prod)
+});
+
+app.get('/api/productosRandom', async (req,res)=>{
+    const prod = await contenedor.getIDRandom()
+    res.json(prod)
+});
+
+//Configuración del puerto del servidor
+
+const PORT = process.env.PORT || 8080
+const server = app.listen(PORT,()=> {
+    console.log(`Escuchando en el puerto: ${server.address().port}`);
+});
+server.on('Ocurrió un error ', error => console.log(error));
