850673213997d9b0cefb379a0e9443e31b30df5d
Aidysteicy <96146121+Aidysteicy@users.noreply.github.com>
2022-11-16 23:28:47 -0300

    commit

--- config.js
+++ config.js
@@ -15,14 +15,14 @@ const config = {
     firebase: {
         "type": "service_account",
         "project_id": "proyectocoderv2",
-        "private_key_id": "f72f82569b1415f4e6ad8efd79ec47e5abd933b2",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCm3ibzt4csyABK\ndYPt8UXoaCNHukfe7ksVT2sGZfVr0niussXnzdvfc+92WB3vDaU5/+QYRRVSfmyF\n9A0sGFhZrcRHorslq+0MMz7a6IXvRnBi1CXD7zeOnhhaKguj19U3ottehpj4vj2A\nAIcW3HlAgtBh+IcZifg+Ze3jCDCS9JH1QRtOt7a2iZVmbiASwyGeys1BYWQFTX3k\nlSdY3vm9U244lfUwGluTWiXmyAoqL7NPbQ42qFpXjcmtqju9MzLNTAofDh6ZThPf\na0I6PGlkQp+3fD2+TRIkHGXnsy69HcMxRhMYOtzK7Pu/DqCGZrLBNsH8FQFoU7ra\nTkA4Skv/AgMBAAECggEAA/NLacalARblBt48VtQ46vUYLLkIvYrXpTQBnR+pzTko\n2T3rOH8jnukgOKWCY+fnSvW3YSm1hQPwLsWl0C7tXIYkGOwN0Sj+TiUibk1F6nG4\n9GYAVOCjaJFukyfeqazQ4+XLmrPGCFF8pDye9hgXGMNgCom3LRAam+OO26qUd84f\nkdCzL3KMKpHvVdy0K/MtuLqSxjTJPJqFKRZBy+uxfBFqkSZpMJic7hTsUgDy+ESN\nPADzs+UlTdmedDavTjonSujTfiNxM3okNCeBQuWyDDSmuNtN/cBIRQREYvjSlVYd\nVqglp4JluC9Mu8DpOCVrrzYXtcsCzBv6Lu0Rm48oyQKBgQDoiFBPefjcF6CYmW6B\n1qyIoKpgwMf58+NY7I8kXjN+k0zLNdIrSMegmY41GZBTjtiU4TVTh2ia02Q7Kogi\niXU2FpGyZVraQ2KNYXrggAW2+j3avPxmoWsvpcRRkydoiBGv5eJyk/zrsWT7r/hi\ng45g/BoaewPAJWSGwy3TzECJpwKBgQC3tVTzvLMJoHX/Og7gC4Ue0wM30hmhR34m\nBg/uUxzFNO6wco6aIm0gVk3lQ3q1VWBGromeVFuu2g7w9pyGxxhnn9sHdTw+qAjI\nhrtuHmBzkJifcHia9eWYB0oR+xSiSwrPGqIxnBN/pceKEvDKlULF/ejGNp19zBaC\nEc4VCFVF6QKBgF9ynKyS34DQPf6FdGPdT4LhmC4Q08UYxChbKcJ+3FpHck0zcWqM\n/ExC26mWLbTuvJ4X90CrI1fyD4FtyuWKUM974iA9FFblNA3ehoaiZjYeb8darawe\n9BVh0Ly/T3zL5Z6zGQl5D2dbsJzTNXySjZ3nvXtDJV34u1PJyATZIWCfAoGBAJKf\nfN1Ih5Ewa904JnU1XJtciZ38543stMd+YSqzVSwk8jkMRbrJrVV47JUGhaLDMKNZ\nXOdspvxoeDJkcku0Dktur+9E57+Od+wsdN4QCqVSDC59IxVzGzt63eh6AF8EC+1+\ncWOtEXb5BsiyE1NtTC0gWbASGSM1R+WqdlQdIYo5AoGAAN/DZfVdL8MJ8jlEXqN2\nUdu2NEH13xHBlBumX+y29ESq68qa+kpHmeLV8unc1QIadc4Qn6sW0W8wvRqPovsX\nY+L1B+v5MkfsMeN/sxv6ehSqNv7CWTU/1XlyGS0hZHlcbI4ZKZbkO4DLTk0+pEE1\nE9kPPLtN2rgA5pftCeGwZ6w=\n-----END PRIVATE KEY-----\n",
-        "client_email": "firebase-adminsdk-jcmg2@proyectocoderv2.iam.gserviceaccount.com",
-        "client_id": "101697466313858418345",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-jcmg2%40proyectocoderv2.iam.gserviceaccount.com"
+        "private_key_id": "",
+        "private_key": "",
+        "client_email": "",
+        "client_id": "5",
+        "auth_uri": "",
+        "token_uri": "",
+        "auth_provider_x509_cert_url": "",
+        "client_x509_cert_url": ""
       },
     mariadb: {
         client: 'mysql',
